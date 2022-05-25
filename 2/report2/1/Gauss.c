c94223b3b67c35f7e287326e630308fa201db935
Jagashira <pyprogjas3104@gmail.com>
2022-05-25 18:08:13 +0900

    Title 配列input output その他の処理途中
    Who Jagashira
    
    Commit
    配列をinput,outputできるように実装
    ガウスの処理の実装の途中
    実行したが、まだうまく処理できていないため、アルゴリズムの変更が必要

--- 2/report2/1/Gauss.c
+++ 2/report2/1/Gauss.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void matlix_display(int row,int col,double *matlix[]);
 
 int main(int argc, char const *argv[]){
 
@@ -14,7 +15,9 @@ int main(int argc, char const *argv[]){
         puts("Enter two arguments on the command line");
         return 1;
     }
-    int matlix[row][col];
+    
+    //配列のinput
+    double matlix[row][col];
     for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
             scanf("%d",&matlix[i][j]);
@@ -22,34 +25,81 @@ int main(int argc, char const *argv[]){
         }
     }
 
-
+    //配列のoutput
     for(int i=0;i<row;i++){
-        if(i==0){
-            printf("/");
-        }else if(i==row-1){
-            printf("\\");
-        }else{
-            printf("|");
-        }
-
+        if(row == 1) printf("|");
+        else if(i==0) printf("/");
+        else if(i==row-1) printf("\\");
+        else printf("|");
+        
         for(int j=0;j<col;j++){
             printf("  %d  ",matlix[i][j]);
         }
 
-        if(i==0){
-            printf("\\");
-        }else if(i==row-1){
-            printf("/");
-        }else{
-            printf("|");
+        if(row == 1) printf("|");
+        else if(i==0) printf("\\");
+        else if(i==row-1) printf("/");
+        else printf("|");
+        
+        puts("");
+    }
+    
+    for(int j=0;j<col;j++){
+        for(int i=0;i<row;i++){
+            double m = matlix[i][j]/matlix[i][0];
+            printf("%lf\n",m);
+            matlix[i][j] /= m;
+            printf("%d %d\n",i,j);
+        }
+    }
+
+    // matlix_display(row,col,&matlix);
+
+    for(int i=0;i<row;i++){
+        if(row == 1) printf("|");
+        else if(i==0) printf("/");
+        else if(i==row-1) printf("\\");
+        else printf("|");
+        
+        for(int j=0;j<col;j++){
+            printf("  %d  ",matlix[i][j]);
         }
 
-
+        if(row == 1) printf("|");
+        else if(i==0) printf("\\");
+        else if(i==row-1) printf("/");
+        else printf("|");
+        
         puts("");
     }
 
 
-    // printf("%d %d\n",row,col);
+    
+
+
     
     return 0;
+}
+
+
+
+void matlix_display(int row,int col,double *matlix[]){
+
+    for(int i=0;i<row;i++){
+        if(row == 1) printf("|");
+        else if(i==0) printf("/");
+        else if(i==row-1) printf("\\");
+        else printf("|");
+        
+        for(int j=0;j<col;j++){
+            printf("  %d  ",matlix[i][j]);
+        }
+
+        if(row == 1) printf("|");
+        else if(i==0) printf("\\");
+        else if(i==row-1) printf("/");
+        else printf("|");
+        
+        puts("");
+    }
 }
