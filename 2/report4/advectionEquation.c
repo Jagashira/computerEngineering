b97e46a70d6546ccd9e3b792a37a119698ccdb92
Jagashira <pyprogjas3104@gmail.com>
2022-07-20 04:08:11 +0900

    Title 問題1-dおわた
    Who Jagashira

--- 2/report4/advectionEquation.c
+++ 2/report4/advectionEquation.c
@@ -1,63 +1,102 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-#include <string.h>
+#define n 4
+#define Time 0
+// void array_display(double array[nx][Time], double count, double dx);
+//移流方程式　問題1 (d)
+FILE *data[n], *gp;
+char *data_file[n];
+// char str1[][14] = {"out1d005.dat", "out1d01.dat", "out1d015.dat", "out1d02.dat"};
+char str1[][14] = {
+    "1-b-0.05.dat",
+    "1-b-0.1.dat",
+    "1-b-0.5.dat",
+};
 
-#define c 2.5
-#define nx 200
-#define nt  1000
-#define dt 0.01
-#define dx 0.1
-
-double init(double x);
-void array_copy(double u[200], double u_old[200]);
-void array_display(double array[200], double count);
-
-int main(){
-
-    double u[nx] = {};
-    double u_old[nx];
-
-    //初期化
-    for (int i = 0; i < nx; i++){
-        u[i] = init(i * dx);
-    }
-    //main
-    for (int i = 0; i < nt; i++){
-        array_copy(u, u_old);
-        for (int j = 1; j < nx - 1; j++){
-            u[j] = (u_old[j] - c * (dt / dx) * (u_old[j] - u[j - 1]));
-        }
-        // printf("%lf\n",dt*i);
-
-        // if (dt*i == 0||dt*i == 0.10000||dt*i == 1.000000||dt*i == 2.000000||dt*i == 4.000000||dt*i == 8.000000){
-        if(i == 10||i == 100||i == 200||i == 400||i == 800){
-            printf("%0.1lf\n",dt*i);
-            array_display(u, nx);
-        }
-    }
-}
-
-double init(double x){
-    printf("%lf\n",x);
-    if (1 > x || x > 3){
+double init(double x)
+{
+    // printf("%lf\n", x);
+    if (1 > x || x > 3)
+    {
         return 0;
-    }else{
+    }
+    else
+    {
         return -(x - 1) * (x - 3);
     }
-
-
 }
+int main()
+{
+    double **u;
+    int i, j, k, p;
+    double c = 2.5;
+    // x,tの刻み幅
+    double dt = 0.01;
+    double dx;
+    // x,tの範囲　0<x<X  0<t<T
+    double T = 10;
+    double X = 20;
+    double count[6] = {0, 0.1, 1, 2, 4, 8};
+    double xcount[3] = {0.05, 0.1, 0.5};
 
+    for (p = 0; p <= 2; p++)
+    {
+        dx = xcount[p];
+        //　x,t の要素数
+        int tn = (T / dt) + 2;
+        int xn = (X / dx) + 1;
+        /*動的にメモリ領域を確保*/
+        u = malloc(sizeof(double *) * (xn + 1));
+        for (i = 0; i <= xn + 1; i++)
+        {
+            u[i] = malloc(sizeof(double) * tn);
+        }
 
-void array_copy(double u[nx], double u_old[nx]){
-    for (int i = 0; i < 200; i++){
-        u_old[i] = u[i];
+        /*t=0 の時の初期条件を代入*/
+        for (int i = 0; i < xn; i++)
+        {
+            u[i][Time] = init(i * dx);
+        }
+        /// array_display(u[i][Time], xn, dx);
+
+        /*u[x][t]からu[x][t+dt]の計算*/
+
+        for (j = 0; j <= tn - 1; j++)
+        {
+            u[0][j + 1] = u[0][j] - c * dt / dx * (u[0][j] - 0);
+            for (i = 1; i <= xn; i++)
+            {
+                u[i][j + 1] = u[i][j] - c * dt / dx * (u[i][j] - u[i - 1][j]);
+            }
+        }
+        printf("%f\n", u[25][0]);
+        printf("%d\n", xn);
+
+        /*------ データファイル作成 ---------- */
+
+        data_file[p] = str1[p];
+        data[p] = fopen(data_file[p], "w");
+        for (k = 0; k <= xn; k++)
+        {
+            fprintf(data[p], "%f\t%f\n", k * dx, u[k][400]);
+        }
+        fclose(data[p]);
+        /*使用済みのメモリ領域を解放*/
+        for (i = 0; i <= xn; i++)
+        {
+            free(u[i]);
+        }
+        free(u);
     }
+    return 0;
 }
 
-void array_display(double array[nx], double count){
-    // 配列　表示
-    for (int i = 0; i < count; i++){
-        printf("%.1lf %lf \n", i*dx, array[i]);
-    }
-}
+// void array_display(double array[nx][Time], double count, double dx)
+// {
+//     // 配列　表示
+//     for (int i = 0; i < count; i++)
+//     {
+//         printf("%.1lf %lf \n", i * dx, array[i][Time]);
+//     }
+// }
